44cca2df9aabb21d48086c5b31d61e4fbfa6b4d5
marian <marian.flaemig@gmail.com>
2025-10-27 10:44:16 +0100

    twig template inheritance

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,1 +1,27 @@
 <?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/')]
+    public function homepage(): Response
+    {
+        $starship = [
+            'name' => 'SS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Lux Pickles',
+            'status' => 'under construction'
+        ];
+
+        $numberOfStarships = 457;
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $numberOfStarships,
+            'starship' => $starship
+        ]);
+    }
+}
